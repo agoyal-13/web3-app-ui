e31e1787234e13b93de076c1a4668d841fbe644d
agoyal <ankitgoyaljmi@gmail.com>
2024-04-24 22:22:48 +0530

    initial react code for the web3 apps

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="p-3 bg-green-400 rounded-xl shadow-2xl">
+        <p>web3 demo</p>
+      </div>
+    </div>
+  );
+}
