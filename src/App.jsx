88c419db4769d6ee4d09fce2ceec93f7d90e00ad
agoyal <ankitgoyaljmi@gmail.com>
2024-05-07 12:29:19 +0530

    app code and css changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
 import About from "./pages/About.jsx";
 import NoPage from "./pages/NoPage.jsx";
+import HoroscopeNFT from "./components/HoroscopeNFT.jsx";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
             <Route path="/home" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
             <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/horoscopeNFT" element={<HoroscopeNFT />}></Route>
             <Route path="*" element={<NoPage />}></Route>
           </Routes>
         </div>
