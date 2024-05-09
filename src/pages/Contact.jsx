49f735b535eac8e22370a380471ea158f014a03a
agoyal <ankitgoyaljmi@gmail.com>
2024-05-10 00:33:56 +0530

    minor changes and the background change

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header.jsx";
+
+export default function Contact() {
+    return (
+        <>
+            <Header />
+            <h2>Contact Page</h2>
+        </>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import "./../scss/main.scss";
 import { Link } from "react-router-dom";
 import Header from "./../components/Header.jsx";
+import Footer from "./../components/Footer.jsx";
 
 const Home = () => {
     return (
@@ -12,13 +13,16 @@ const Home = () => {
                     <nav>
                         <ul>
                             <li><Link to="/">Home</Link></li>
-                            <li><Link to="/about">Lottery</Link></li>
+                            <li><Link to="/horoscopeNFT">Horoscope NFT</Link></li>
                             <li><Link to="/voting">Voting</Link></li>
                         </ul>
                     </nav>
 
                 </div>
             </div>
+            <div><Footer /></div>
+            <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+            <script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
         </>
     );
 };
