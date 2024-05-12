a485559be35efaf896a4d844c26ff90c015cd282
agoyal <ankitgoyaljmi@gmail.com>
2024-05-12 12:29:15 +0530

    header footer refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ export default function Footer() {
     return (
         <>
             <div className="panel-footer">
-                This is a test application for demo purposes and please use the testnet accounts only for testing
+                <p>This is a test application for demo purposes and please use the testnet accounts only for demo</p>
+                <p>All rights reserved to ankit goyal</p>
+
             </div>
         </>
     );
